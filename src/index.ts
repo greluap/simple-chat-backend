a9f999f52376047c8fe3610e6e3eb78a7f842282
greluap <greluap@gmail.com>
2026-02-10 08:58:02 +0100

    db integration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
 import { Elysia } from "elysia";
+import { MessageModel } from "./model";
 
-new Elysia()
-    .get('/ping', 'peng')
-    .listen(3000)
+type Message = {
+  sender: string;
+  content: string;
+};
+
+let messages: Message[] = [];
+
+new Elysia({prefix: '/messages'})
+  .post(
+    "/",
+    ({ body, set }) => {
+      body: body;
+
+      if (body.content && body.sender != undefined && null) {
+        const message = {
+          sender: body.sender,
+          content: body.content,
+        };
+
+        messages.push(message);
+
+        set.status = 201;
+        return message;
+      } else {
+        set.status = 400;
+        return null;
+      }
+    },
+    { body: MessageModel.MessageSendBody },
+  )
+  .get("/", ({ set }) => {
+    set.status = 200;
+    return messages;
+  })
+  .listen(3000);
